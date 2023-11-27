2d647cbf4e45d2347ace2089ce7033e195435780
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2023-11-27 08:35:52 +0100

    Update ZadaniaGroupBy.sql

--- INF/SQL/ZadaniaGroupBy.sql
+++ INF/SQL/ZadaniaGroupBy.sql
@@ -1,17 +1,17 @@
 BAZA --> AWBackup
 
 # 1. Policz ile jest wszystkich kart
-  SELECT COUNT(*) FROM creditcard;
+	SELECT COUNT(*) FROM creditcard;
 
 # 2. Policz ile kart wygasa w 2008
-  SELECT ExpYear, COUNT(*) FROM creditcard
-  GROUP BY ExpYear
-  HAVING ExpYear = 2008;
+  	SELECT ExpYear, COUNT(*) FROM creditcard
+ 	GROUP BY ExpYear
+  	HAVING ExpYear = 2008;
 
 # 3. Policz ile kard wygasa w 2008 roku w październiku
-  SELECT ExpYear, ExpMonth, COUNT(*) FROM creditcard
-  GROUP BY ExpMonth
-  HAVING ExpYear = 2008 AND ExpMonth = 10;
+  	SELECT ExpYear, ExpMonth, COUNT(*) FROM creditcard
+ 	GROUP BY ExpMonth
+  	HAVING ExpYear = 2008 AND ExpMonth = 10;
 
 # 4. Policz ilość kart Vista
 	SELECT CardType, COUNT(CardType) FROM creditcard
@@ -26,7 +26,7 @@ BAZA --> AWBackup
 	ORDER BY COUNT(*) DESC;
 	
 # 6. Wypisz 3 najliczniejsze miesiące kiedy wygasają karty Vista w 2007 roku.
-  SELECT ExpMonth, COUNT(*) FROM creditcard
+ 	SELECT ExpMonth, COUNT(*) FROM creditcard
 	WHERE CardType = "Vista" AND ExpYear = 2007
 	GROUP BY ExpMonth
 	ORDER BY 2 DESC
